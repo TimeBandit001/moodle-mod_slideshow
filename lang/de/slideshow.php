d09c502a05930af02a9d548d33094787b11eb6bd
Johannes Albert <ja@albertmedia.com>
2013-08-09 01:14:26 +0200

    Add German translation

---
+++ lang/de/slideshow.php
@@ -0,0 +1,94 @@
+<?php
+
+//
+$string['modulename'] = 'Diashow';
+$string['pluginname'] = 'Diashow';
+$string['modulenameplural'] = 'Diashows';
+$string['pluginadministration'] = 'Diashow';
+$string['slideshowsfound'] = 'Diashows in Kurs {$a}';
+
+// for mod.html
+$string['maindirectory'] = 'Bilddateien';
+$string['no_GD_no_thumbs'] = 'Die PHP-Erweiterung "GD" ist nicht installiert, daher können keine Vorschaubilder erstellt werden.';
+$string['display_filename'] = 'Dateiname/Beschriftung anzeigen?';
+$string['display_none'] = 'Nicht anzeigen';
+$string['display_over'] = 'Über dem Bild';
+$string['display_under'] = 'Unter dem Bild';
+$string['display_right'] = 'Rechts vom Bild';
+$string['thumbnail_layout'] = 'Vorschaubilder anzeigen?';
+$string['onblack'] = 'Schwarzer Hintergrund?';
+$string['centred'] = 'Bilder zentrieren?';
+$string['showtime'] = 'Abspielverzögerung:';
+$string['noautoplay'] = 'Nicht abspielen';
+$string['htmlcaptions'] = 'HTML-Editor für Beschriftungen?';
+$string['comments'] = 'Kommentarfunktion aktivieren?';
+
+// for view.php
+$string['none_found'] = 'Keine Bilder im Hauptverzeichnis gefunden';
+$string['dir_problem'] = 'Es ist ein unbekannter Fehler mit dem angegebenen Verzeichnis aufgetreten';
+$string['open_new'] = 'Druckverson (neues Fenster)';
+$string['warning'] = 'WARNUNG!';
+$string['edit_captions'] = 'Beschriftungen bearbeiten';
+
+// for captions.php
+$string['captiontext'] = 'Sie können Fließtext und HTML-Code ins Beschriftungsfeld einfügen. <p>Um Ihre Änderungen zu übernehmen, klicken Sie bitte auf die &quot;Änderungen speichern&quot;-Schaltfläche am Ende der Seite.';
+$string['captionedit'] = 'Beschriftungen bearbeiten';
+$string['save_changes'] = 'Änderungen speichern';
+$string['saved'] = 'Ihre Beschriftungen wurden gespeichert.';
+$string['continue'] = 'Weiter';
+$string['autopopup'] = 'Automatisch abspielen (Popup)';
+$string['caption'] = 'Beschriftung';
+$string['title'] = 'Titel';
+$string['noauth'] = 'Sie haben nicht die erforderliche Berechtigung, um Beschriftungen zu bearbeiten.';
+
+// for comments
+$string['comment'] = 'Kommentar';
+$string['comment_insert_error'] = 'Beim Speichern des Kommentars ist ein Fehler aufgetreten.';
+$string['comment_add'] = 'Kommentar hinzufügen';
+$string['comment_instructions'] = 'Kommentare sind öffentlich und können von allen Nutzern gelesen werden.';
+$string['comments_header'] = 'Kommentare';
+$string['comments_not_allowed'] = 'Die Kommentarfunktion ist derzeit deaktiviert.';
+
+// for media
+$string['media'] = 'Video';
+$string['media_add'] = 'Video zu dieser Folie hinzufügen';
+$string['media_instructions'] = 'Fügen Sie die Internetadresse eines YouTube- oder Vimeo-Videos ins Feld "URL" ein. Sie können Breite und Höhe des Videos definieren, sowie per Drag&Drop dessen Platzierung vornehmen.';
+$string['media_header'] = 'Video';
+$string['media_insert_error'] = 'Beim Speichern der Videodaten ist ein Fehler aufgetreten.';
+$string['media_update_error'] = 'Beim Aktualisieren der Videodaten ist ein Fehler aufgetreten.';
+$string['media_edit_url'] = 'URL';
+$string['media_edit_x'] = 'X-Ausrichtung';
+$string['media_edit_y'] = 'Y-Ausrichtung';
+$string['media_edit_width'] = 'Breite';
+$string['media_edit_height'] = 'Höhe';
+$string['media_edit_position'] = 'Ziehen Sie diese Box an die gewünschte Position';
+ 
+// for config.html
+$string['configmaxwidth'] = 'Maximale Breite der Bilder (in Pixeln)';
+$string['configmaxheight'] = 'Maximale Höhe der Bilder (in Pixeln)';
+$string['keeporiginals'] = 'Originalbilder erhalten?<br />Falls "Ja" wird in der Druckversion eines Bildes jeweils das unverkleinerte Originalbild dargestellt. Falls "Nein" werden die Originalbilder nach dem Verkleinern verworfen (empfehlenswert bei knappem Speicherplatz).';
+$string['securepix'] = 'Erschwert via JavaScript den Download der Bilder und entfernt den Direktlink.';
+
+$string['modulename_help'] = 'Mit dem Diashow-Modul können Trainer/-innen (kommentierbare) Bilder und Videos in Form einer Diashow bereit stellen.
+
+Auf Ihrem Computer:
+
+* Falls Sie eine bestehende Präsentation enbinden wollen: Exportieren Sie sie in Einzelbilder (z.B. über "Export nach HTML").
+* Fassen Sie die betreffenden Bilder in einer ZIP-Datei zusammen.
+
+In Moodle:
+
+* Fügen Sie die Aktivität "Diashow" zum Kursraum hinzu.
+* Laden Sie die ZIP-Datei hoch und entpacken Sie sie.
+
+Verwendung:
+
+Navigieren Sie per Klick auf die Vorschaubilder. Klicken Sie auf ein Bild, um das darauffolgende Bild anzuzeigen. Ihre aktuelle Position in der Diashow bleibt zwischen Ihren Moodle-Besuchen erhalten. Jede Folie kann mit Kommentaren versehen, sowie einem YouTube- oder Vimeo-Video ergänzt werden.
+
+Benutzer/-innen mit der Rolle "Trainer/-in" (oder darüber) können
+
+* Titel und Beschriftung jedes Bildes festlegen
+* die Platzierung von Vorschaubildern und Beschriftungen definieren
+* einen automatischen Abspielmodus aktivieren
+* den Download der eingebundenen Bilder für Teilnehmer/-innen erschweren';
+
